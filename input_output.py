85d1c579dc8ff7ec4e8068a53bfeb0b3227d9c0a
fmondaca <f.mondaca@uni-koeln.de>
2018-11-05 10:42:34 +0100

    write and sort set

--- input_output.py
+++ input_output.py
@@ -1,9 +1,22 @@
 import pickle
+import pyuca
 
 
-def write_dict(filename, beolingus_dict):
-    with open(filename, mode='w', encoding='utf-8') as f:
-        for k, v in beolingus_dict.items():
+def sort_set(set_to_sort):
+    coll = pyuca.Collator()
+    set_to_sort = sorted(set_to_sort, key=coll.sort_key)
+    return set_to_sort
+
+
+def write_set(filename, set_to_write):
+    with open(filename, mode='wt', encoding='utf-8') as f:
+        for e in set_to_write:
+            f.write(str(e) + '\n')
+
+
+def write_dict(filename, dict_to_write):
+    with open(filename, mode='wt', encoding='utf-8') as f:
+        for k, v in dict_to_write.items():
             f.write(str(k) + '\t' + str(v) + '\n')
 
 
