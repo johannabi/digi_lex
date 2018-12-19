bab4e5d63be361fb0c630a28e3d749be1bfb7055
fmondaca <f.mondaca@uni-koeln.de>
2018-12-19 21:13:17 +0100

    desc (note) gone

--- transform_beolingus.py
+++ transform_beolingus.py
@@ -1,10 +1,6 @@
 import re
 from lxml import etree
-
-
-def pre_process_beo(beo_as_dict):
-    # we need to replace all the ';' between brackets. we chose a '|' as replacement.
-    return beo_as_dict
+import input_output
 
 
 def transform_in_tei(beo_as_dict):
@@ -32,7 +28,6 @@ def transform_in_tei(beo_as_dict):
 
     usg_pattern = re.compile(r'(\[(.*?)\])')
     gramm_pattern = re.compile(r'(\{(.*?)\})')
-    desc_pattern = re.compile(r'(\((.*?)\))')
 
     for k1, v1 in beo_as_dict.items():
 
@@ -46,13 +41,16 @@ def transform_in_tei(beo_as_dict):
             splitted_forms = k2.split(';')
             splitted_senses = v2.split(';')
 
-            for f in splitted_forms:
+            for i, f in enumerate(splitted_forms):
+                i += 1
                 form = etree.SubElement(entry, 'form')
+                form.attrib['{http://www.w3.org/XML/1998/namespace}id'] = "en_{}_f_{}".format(str(k1), i)
+
                 orth = etree.SubElement(form, 'orth')
+                orth.attrib['{http://www.w3.org/XML/1998/namespace}lang'] = "de"
 
                 gramm_matches = gramm_pattern.findall(f)
                 usg_matches = usg_pattern.findall(f)
-                desc_matches = desc_pattern.findall(f)
 
                 # we assume , and only allow one gram per form
                 if len(gramm_matches) > 0:
@@ -67,47 +65,40 @@ def transform_in_tei(beo_as_dict):
                         usg.text = match[1]
                         f = f.replace(match[0], '')
 
-                if len(desc_matches) > 0:
-                    for match in desc_matches:
-                        note = etree.SubElement(form, 'note')
-                        note.text = match[1]
-                        f = f.replace(match[0], '')
-
                 # f = input_output.clean_up_str(f)
-                orth.text = f
-            for s in splitted_senses:
+                orth.text = f.strip()
+            for i, s in enumerate(splitted_senses):
+                i += 1
+
                 sense = etree.SubElement(entry, 'sense')
+                sense.attrib['{http://www.w3.org/XML/1998/namespace}id'] = "en_{}_s_{}".format(str(k1), i)
+                sense.attrib['{http://www.w3.org/XML/1998/namespace}lang'] = "en"
+
                 usg_matches = usg_pattern.findall(s)
-                desc_matches = desc_pattern.findall(s)
+
+                definition = etree.SubElement(sense, 'def')
 
                 if len(usg_matches) > 0:
                     for match in usg_matches:
                         usg = etree.SubElement(sense, 'usg')
                         usg.text = match[1]
                         s = s.replace(match[0], '')
-
-                if len(desc_matches) > 0:
-                    for match in desc_matches:
-                        note = etree.SubElement(sense, 'note')
-                        note.text = match[1]
-                        s = s.replace(match[0], '')
-
-                definition = etree.SubElement(sense, 'def')
                 # s = input_output.clean_up_str(s)
-                definition.text = s
+                definition.text = s.strip()
 
             if len(v1) > 1:
                 super_entry.append(entry)
-                super_entry.attrib['n'] = str(k1)
+                super_entry.attrib['{http://www.w3.org/XML/1998/namespace}id'] = "en_{}".format(str(k1))
+
                 body.append(super_entry)
 
             else:
-                entry.attrib['n'] = str(k1)
+                entry.attrib['{http://www.w3.org/XML/1998/namespace}id'] = "en_{}".format(str(k1))
                 body.append(entry)
 
     et = etree.ElementTree(root)
     return et
 
 
-# et = transform_in_tei(input_output.deserialize('data/splitted_beolingus.pickle'))
-# et.write('data/beolingus_tei_2.xml', pretty_print=True, xml_declaration=True, encoding='utf-8')
+et = transform_in_tei(input_output.deserialize('data/splitted_beolingus_prepro.pickle'))
+et.write('data/beolingus_tei_2.xml', pretty_print=True, xml_declaration=True, encoding='utf-8')
